3db7dca15711d0bc267d5d5ee562675bba013c1f
Manu <mmasenlle@lortek.es>
2019-02-04 09:02:00 +0100

    add binaries

--- enip/opener_dll/enip_adapter.py
+++ enip/opener_dll/enip_adapter.py
@@ -3,10 +3,12 @@ import time,struct,ctypes
 
 
 opener = ctypes.cdll.LoadLibrary('Opener.dll')
+# opener = ctypes.cdll.LoadLibrary('Opener.so')
 opener.config_assemblies(151,20,100,32,150,32)
 # opener.set_print_debug(0) # to quiet verbosity
 opener.get_data_output.restype=ctypes.c_void_p
-opener.init_loop(9)
+opener.init_loop(9) # net interface on windows
+# opener.init_loop('eno1')
 
 while True:
     OT_data = struct.unpack('i'*8, bytearray(ctypes.cast(opener.get_data_output(), ctypes.POINTER(ctypes.c_ubyte * 32)).contents[:]))
