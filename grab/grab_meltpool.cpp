0db9c16d95adff8ae04f907af0604db50904db1e
Manuel Masenlle <mmasenlle@lortek.es>
2019-11-07 13:29:54 +0100

    clean some comments

--- grab/grab_meltpool.cpp
+++ grab/grab_meltpool.cpp
@@ -1,20 +1,3 @@
-// Grab.cpp
-/*
-Note: Before getting started, Basler recommends reading the Programmer's Guide topic
-in the pylon C++ API documentation that gets installed with pylon.
-If you are upgrading to a higher major version of pylon, Basler also
-strongly recommends reading the Migration topic in the pylon C++ API documentation.
-This sample illustrates how to grab and process images using the CInstantCamera class.
-The images are grabbed and processed asynchronously, i.e.,
-while the application is processing a buffer, the acquisition of the next buffer is done
-in parallel.
-The CInstantCamera class uses a pool of buffers to retrieve image data
-from the camera device. Once a buffer is filled and ready,
-the buffer can be retrieved from the camera object for processing. The buffer
-and additional image data are collected in a grab result. The grab result is
-held by a smart pointer after retrieval. The buffer is automatically reused
-when explicitly released or when the smart pointer object is destroyed.
-*/
 
 #include <pylon/PylonIncludes.h>
 #ifdef PYLON_WIN_BUILD
@@ -33,14 +16,6 @@ when explicitly released or when the smart pointer object is destroyed.
 #include <mutex>
 #include <condition_variable>
 
-//using namespace cv;
-//
-//// Namespace for using pylon objects.
-//using namespace Pylon;
-//
-//
-//// Namespace for using cout.
-//using namespace std;
 
 // Number of images to be grabbed.
 static const uint32_t c_countOfImagesToGrab = 100;
