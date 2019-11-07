4aadf35f43b9af482341a2ce33b1281978da4574
Manuel Masenlle <mmasenlle@lortek.es>
2019-11-07 13:27:35 +0100

    grab images and calculate area in pixels of meltpool

---
+++ grab/grab_meltpool.cpp
@@ -0,0 +1,212 @@
+// Grab.cpp
+/*
+Note: Before getting started, Basler recommends reading the Programmer's Guide topic
+in the pylon C++ API documentation that gets installed with pylon.
+If you are upgrading to a higher major version of pylon, Basler also
+strongly recommends reading the Migration topic in the pylon C++ API documentation.
+This sample illustrates how to grab and process images using the CInstantCamera class.
+The images are grabbed and processed asynchronously, i.e.,
+while the application is processing a buffer, the acquisition of the next buffer is done
+in parallel.
+The CInstantCamera class uses a pool of buffers to retrieve image data
+from the camera device. Once a buffer is filled and ready,
+the buffer can be retrieved from the camera object for processing. The buffer
+and additional image data are collected in a grab result. The grab result is
+held by a smart pointer after retrieval. The buffer is automatically reused
+when explicitly released or when the smart pointer object is destroyed.
+*/
+
+#include <pylon/PylonIncludes.h>
+#ifdef PYLON_WIN_BUILD
+#include <pylon/PylonGUI.h>
+#endif
+
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/core/core.hpp"
+#include <pylon/PylonImage.h>
+#include <pylon/Pixel.h>
+#include <pylon/ImageFormatConverter.h>
+#include <pylon/gige/BaslerGigEInstantCamera.h>
+#include <chrono>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+//using namespace cv;
+//
+//// Namespace for using pylon objects.
+//using namespace Pylon;
+//
+//
+//// Namespace for using cout.
+//using namespace std;
+
+// Number of images to be grabbed.
+static const uint32_t c_countOfImagesToGrab = 100;
+
+static int req_fps = 100, thresh = 50;
+static int show_img = 0, nthreads = 1;
+
+class MeltpoolCalculer
+{
+	int id;
+	int cnt;
+	int d;
+	cv::Mat org, gray, bin;
+	Pylon::CPylonImage image;
+	Pylon::CImageFormatConverter fc;
+public:
+	MeltpoolCalculer(int _id, int w, int h) {
+		id = _id;
+		cnt = 0;
+		fc.OutputPixelFormat = Pylon::PixelType_RGB8packed;
+		org = cv::Mat(w, h, CV_8UC3);
+		gray = cv::Mat(w, h, CV_8UC3);
+		bin = cv::Mat(w, h, CV_8UC3);
+		d = req_fps;
+		if (nthreads > 1) d = req_fps / (nthreads - 1);
+	};
+	int get_mp(Pylon::CGrabResultPtr ptrGrabResult) {
+		fc.Convert(image, ptrGrabResult);
+		org = cv::Mat(ptrGrabResult->GetHeight(), ptrGrabResult->GetWidth(), CV_8UC3, (uint8_t*)image.GetBuffer());
+		cv::cvtColor(org, gray, cv::COLOR_BGR2GRAY);
+		cv::threshold(gray, bin, thresh, 1, cv::THRESH_BINARY);
+		int meltpool = cv::sum(bin)[0];
+
+		if (id == 1 && show_img) {
+			cv::imshow("CV_Image", org);
+			cv::waitKey(1);
+		}
+
+		if (++cnt % d == 0)
+			std::cout << "MeltpoolCalculer(" << id << ") " << cnt << " mp: " << meltpool << std::endl;
+		
+		return meltpool;
+	};
+	
+};
+
+std::mutex mtx;
+std::condition_variable cond;
+std::list<Pylon::CGrabResultPtr> images;
+std::vector<std::thread> workers;
+bool keep_running = true;
+int g_cnt = 0;
+
+void worker(int id, int w, int h) {
+	MeltpoolCalculer mc(id, w, h);
+	while (keep_running) {
+		std::unique_lock<std::mutex> lk(mtx);
+		if (images.empty()) cond.wait(lk);
+		if (!images.empty()) {
+			Pylon::CGrabResultPtr gr = images.front();
+			images.pop_front();
+			g_cnt++;
+			lk.unlock();
+			mc.get_mp(gr);
+		}
+	}
+}
+
+
+int main(int argc, char* argv[])
+{
+	// The exit code of the sample application.
+	int exitCode = 0;
+
+	if (argc > 1 && isdigit(argv[1][0])) {
+		req_fps = atoi(argv[1]);
+	}
+	if (argc > 2 && isdigit(argv[2][0])) {
+		thresh = atoi(argv[2]);
+	}
+	if (argc > 3 && isdigit(argv[3][0])) {
+		show_img = atoi(argv[3]);
+	}
+	if (argc > 4 && isdigit(argv[4][0])) {
+		nthreads = atoi(argv[4]);
+	}
+	std::cout << "req_fps: " << req_fps << " / thresh: " << thresh << " / show_img: " << show_img << " / nthreads: " << nthreads << std::endl;
+
+	// Automagically call PylonInitialize and PylonTerminate to ensure
+	// the pylon runtime system is initialized during the lifetime of this object.
+	Pylon::PylonAutoInitTerm autoInitTerm;
+
+
+	if (show_img) cv::namedWindow("CV_Image", cv::WINDOW_AUTOSIZE);
+	try
+	{
+
+		//Pylon::CInstantCamera camera(Pylon::CTlFactory::GetInstance().CreateFirstDevice());
+		Pylon::CBaslerGigEInstantCamera camera(Pylon::CTlFactory::GetInstance().CreateFirstDevice());
+		//Pylon::CBaslerGigEInstantCamera 
+		std::cout << "Using device " << camera.GetDeviceInfo().GetModelName() << std::endl;
+		camera.Open();
+
+		camera.TriggerMode.SetValue(Basler_GigECamera::TriggerMode_Off);
+		camera.AcquisitionFrameRateEnable = 1;
+		camera.AcquisitionFrameRateAbs = req_fps;
+		camera.ExposureTimeRaw = 100;
+
+		camera.GevSCPSPacketSize.SetValue(1500);
+		camera.GevSCPD.SetValue(0);
+
+		GenApi::CIntegerPtr width(camera.GetNodeMap().GetNode("Width"));
+		GenApi::CIntegerPtr height(camera.GetNodeMap().GetNode("Height"));
+
+		MeltpoolCalculer mc(1, width->GetValue(), height->GetValue());
+
+		for (int i = 1; i < nthreads; i++) {
+			workers.push_back(std::thread(worker, i, width->GetValue(), height->GetValue()));
+		}
+
+		camera.StartGrabbing();
+
+
+		int i = 0;
+		auto t0 = std::chrono::system_clock::now();
+
+		while (camera.IsGrabbing()) {
+			Pylon::CGrabResultPtr ptrGrabResult;
+			camera.RetrieveResult(500, ptrGrabResult, Pylon::TimeoutHandling_ThrowException);
+			if (ptrGrabResult->GrabSucceeded()) {
+				
+				if (nthreads < 2) {
+					double meltpool = mc.get_mp(ptrGrabResult);
+				}
+				else {
+					std::lock_guard<std::mutex> guard(mtx);
+					images.push_back(ptrGrabResult);
+					cond.notify_one();
+				}
+
+				if (++i % req_fps == 0) {
+					//i = 0;
+					std::chrono::duration<double> secs = std::chrono::system_clock::now() - t0;
+					std::cout << "Grabbed/Processed:" << i << "/" << g_cnt << " fps: "
+						<< (double(req_fps) / secs.count()) << std::endl;
+					t0 = std::chrono::system_clock::now();
+				}
+			}
+		}
+
+	}
+
+	catch (GenICam::GenericException &e)
+	{
+		// Error handling.
+		std::cerr << "An exception occurred." << std::endl
+			<< e.GetDescription() << std::endl;
+		exitCode = 1;
+	}
+
+	keep_running = false;
+	cond.notify_all();
+
+	// Comment the following two lines to disable waiting on exit
+	std::cerr << std::endl << "Press Enter to exit." << std::endl;
+	while (std::cin.get() != '\n');
+
+	return exitCode;
+}
